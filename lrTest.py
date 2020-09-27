89cd7ac022921629e1514a92d91b200b46445502
jsprute <jesprl@hotmail.com>
2020-09-27 17:34:04 -0400

    RG and RLog

---
+++ lrTest.py
@@ -0,0 +1,12 @@
+#import  required libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.linear_model import linearRegression
+
+# generate the random dataset
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+
+
+
