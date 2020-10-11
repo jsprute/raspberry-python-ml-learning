cb7d8228ee896f61478f802c27ae5453caf6fa56
jsprute <jesprl@hotmail.com>
2020-10-11 18:46:17 -0400

    decision trees demo

---
+++ treeSelectDemo2.py
@@ -0,0 +1,48 @@
+# Load all required libraries
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO
+from sklearn import tree
+import pydotplus
+import collections
+
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+
+# Load dataset
+pima = pd.read_csv("./data/diabetes.csv", header=None, names=col_names)
+
+#split dataset in features and target variable
+feature_cols = ['pregnant', 'insulin', 'bmi', 'age', 'glucose', 'bp', 'pedigree']
+X = pima[feature_cols] # Features
+y = pima.label # Target variable
+
+# Split dataset into training set and test set
+# 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+# Create Decision Tree classifer object
+#clf = DecisionTreeClassifier()
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
+dot_data = tree.export_graphviz(clf, out_file=None, filled=True, rounded=True, special_characters=True, feature_names= feature_cols, class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data)
+
+colors = ('turquoise', 'orange')
+
+edges = collections.defaultdict(list)
+for edge in graph.get_edge_list():
+    edges[edge.get_source()].append(int(edge.get_destination()))
+
+for edge in edges:
+    edges[edge].sort()
+    for i in range(2):
+        dest = graph.get_node(str(edges[edge][i]))[0]
+        dest.set_fillcolor(colors[i])
+
+graph.write_png('tree.png')
+
+
