777de519135abd985eafb1c105db4d918788f54a
Jason S <jesprl@hotmail.com>
2021-01-16 19:12:17 -0500

    initial commit for diabetestest

---
+++ diabetesTest.py
@@ -0,0 +1,8 @@
+# Import required libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as import pd
+
+# Load the CSV dataset
+dataset = pd.read_csv('./data/diabetes.csv')
+dataset.head(10)
